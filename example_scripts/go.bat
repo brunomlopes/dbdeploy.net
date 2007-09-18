79e0535d2f76197973fd8698f4f3b0ea61a65109
trgoodwin <trgoodwin@344d1307-1f30-0410-b0d9-920a146ec657>
2007-09-18 18:35:02 +0000

    modified build to make dist include example and scripts as well. updated AssemblyInfo.cs to reflect 1.0 release modified the example_scripts to be more intuitive.
    git-svn-id: https://dbdeploy-net.svn.sourceforge.net/svnroot/dbdeploy-net/trunk@18 344d1307-1f30-0410-b0d9-920a146ec657

--- example_scripts/go.bat
+++ example_scripts/go.bat
@@ -1,10 +1,2 @@
 @ECHO OFF
-
-IF "%1" == "" (
-	Tools\nant\bin\nant.exe -buildfile:test.build
-) ELSE IF "%1" == "help" (
-	Tools\nant\bin\nant.exe -buildfile:test.build -projecthelp
-) ELSE (
-	Tools\nant\bin\nant.exe -buildfile:test.build %*
-)
-
+Tools\nant\bin\nant.exe -buildfile:example.build
--- src/Net.Sf.Dbdeploy/Properties/AssemblyInfo.cs
+++ src/Net.Sf.Dbdeploy/Properties/AssemblyInfo.cs
@@ -1,26 +1,28 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Net.Sf.Dbdeploy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ThoughtWorks, Inc")]
-[assembly: AssemblyProduct("Net.Sf.Dbdeploy")]
-[assembly: AssemblyCopyright("Copyright © ThoughtWorks, Inc 2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+[assembly : AssemblyTitle("Net.Sf.Dbdeploy")]
+[assembly : AssemblyDescription("")]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("ThoughtWorks, Inc")]
+[assembly : AssemblyProduct("Net.Sf.Dbdeploy")]
+[assembly : AssemblyCopyright("Copyright © ThoughtWorks, Inc 2007")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+
+[assembly : ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e9c5c1d5-7801-426e-8616-ac046f46dc91")]
+
+[assembly : Guid("e9c5c1d5-7801-426e-8616-ac046f46dc91")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+
+[assembly : AssemblyVersion("1.0.0.0")]
+[assembly : AssemblyFileVersion("1.0.0.0")]
