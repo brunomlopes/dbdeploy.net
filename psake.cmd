03d276a351e5bf156aa6cd362a6f399a20f399de
brunomlopes <brunomlopes@gmail.com>
2010-09-16 15:44:57 +0100

    Psake script to build distribution package

---
+++ psake.cmd
@@ -0,0 +1,2 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "psake.ps1 %*"
--- scripts/createSchemaVersionTable.mssql.sql
+++ scripts/createSchemaVersionTable.mssql.sql
@@ -1,6 +1,3 @@
-USE dbdeploy
-GO
-
 CREATE TABLE changelog (
   change_number INTEGER NOT NULL,
   delta_set VARCHAR(10) NOT NULL,
