bc89f18696577a0187188b1f138ac701399a70cf
Ruano Martinez Schulze <ruano@inventti.com.br>
2014-03-13 17:53:51 -0300

    Changes to support Firebird 1.5

---
+++ src/Test.Net.Sf.Dbdeploy/Database/DbmsFactoryFirebirdTeste.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Net.Sf.Dbdeploy.Database
+{
+    class DbmsFactoryFirebirdTeste : DbmsFactoryAbstractTest
+    {
+        private const string FirebirdConnectionString = "User=SYSDBA;Password=masterkey;Database=C:\\DBDEPLOY.fdb;DataSource=localhost;Port=3050;";
+        private const string DbmsFirebird = "firebird";
+        private const string FirebirdDllName = "FirebirdSql.Data.FirebirdClient.dll";
+        private readonly string firebirdDllPath = AppDomain.CurrentDomain.BaseDirectory + "\\Mocks\\Fixtures\\FirebirdDllConnection\\" + FirebirdDllName;
+
+        [Test]
+        public override void GivenAnEntryWithoutDllConnectionShouldThrowsException()
+        {
+            var factory = new DbmsFactory(DbmsFirebird, FirebirdConnectionString);
+            var connection = OpenConnection(factory);
+            Assert.AreEqual(ConnectionState.Open, connection.State);
+            CloseConnection();
+        }
+
+        [Test]
+        public override void GivenAnEntryWithDllConnectionShouldOpenedSuccessful()
+        {
+            var factory = new DbmsFactory(DbmsFirebird, FirebirdConnectionString, firebirdDllPath);
+            var connection = OpenConnection(factory);
+            Assert.AreEqual(ConnectionState.Open, connection.State);
+            CloseConnection();
+        }
+
+        [Test]
+        public override void ShouldExecuteSqlCommandSuccessful()
+        {
+            var factory = new DbmsFactory(DbmsFirebird, FirebirdConnectionString, firebirdDllPath);
+            OpenConnection(factory);
+            const string query = "SELECT * FROM rdb$relation_fields";
+            ExecuteNonQuery(query);
+            CloseConnection();
+        }
+
+        [Test]
+        [ExpectedException("FirebirdSql.Data.FirebirdClient.FbException")]
+        public override void ShouldExecuteSqlCommandFailed()
+        {
+            var factory = new DbmsFactory(DbmsFirebird, FirebirdConnectionString, firebirdDllPath);
+            OpenConnection(factory);
+            const string query = "SELECT * FROM ANY_TABLE";
+            ExecuteNonQuery(query);
+            CloseConnection();
+        }
+    }
+}
---
+++ src/Test.Net.Sf.Dbdeploy/Database/DbmsFactoryMySqlTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Net.Sf.Dbdeploy.Database
+{
+    class DbmsFactoryMySqlTest : DbmsFactoryAbstractTest
+    {
+        private readonly string mySqlDllPath = AppDomain.CurrentDomain.BaseDirectory + "\\Mocks\\Fixtures\\MySqlDllConnection\\" + MySqlDllName;
+        private const string DbmsMySql = "mysql";
+        private const string MySqlDllName = "MySql.Data.dll";
+        private const string MySqlConnectionString = "Server=localhost; Database=dbdeploy; Uid=dbdeploy; Pwd=dbdeploy;";
+
+        [Test]
+        [ExpectedException("System.IO.FileNotFoundException")]
+        public override void GivenAnEntryWithoutDllConnectionShouldThrowsException()
+        {
+            var factory = new DbmsFactory(DbmsMySql, MySqlConnectionString);
+            var connection = OpenConnection(factory);
+            Assert.AreEqual(ConnectionState.Open, connection.State);
+            CloseConnection();
+        }
+
+        [Test]
+        public override void GivenAnEntryWithDllConnectionShouldOpenedSuccessful()
+        {
+            var factory = new DbmsFactory(DbmsMySql, MySqlConnectionString, mySqlDllPath);
+            var connection = OpenConnection(factory);
+            Assert.AreEqual(ConnectionState.Open, connection.State);
+            CloseConnection();
+        }
+
+        [Test]
+        public override void ShouldExecuteSqlCommandSuccessful()
+        {
+            var factory = new DbmsFactory(DbmsMySql, MySqlConnectionString, mySqlDllPath);
+            OpenConnection(factory);
+            const string query = "Select CURRENT_TIMESTAMP From Dual;";
+            ExecuteNonQuery(query);
+            CloseConnection();
+        }
+
+        [Test]
+        [ExpectedException("MySql.Data.MySqlClient.MySqlException")]
+        public override void ShouldExecuteSqlCommandFailed()
+        {
+            var factory = new DbmsFactory(DbmsMySql, MySqlConnectionString, mySqlDllPath);
+            OpenConnection(factory);
+            const string query = "Select ANY_TABLE From Dual;";
+            ExecuteNonQuery(query);
+            CloseConnection();
+        }
+    }
+}
