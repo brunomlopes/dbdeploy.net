dda4fef62999a95211980cb7c37ae204eeaa4018
Ruano Martinez Schulze <ruano@inventti.com.br>
2014-03-17 09:49:09 -0300

    Changes to support Firebird 1.5

--- src/SolutionVersionAssemblyInfo.cs
+++ src/SolutionVersionAssemblyInfo.cs
@@ -1,1 +1,1 @@
-// Do not commit the changes to this file.using System.Reflection;[assembly: AssemblyVersionAttribute("2.1.0.0")][assembly: AssemblyInformationalVersionAttribute("2.1.0.0-7-c68ea866f-20140307_1733")][assembly: AssemblyFileVersionAttribute("2.1.0.0")]
+// Do not commit the changes to this file.using System.Reflection;[assembly: AssemblyVersionAttribute("2.1.0.0")][assembly: AssemblyInformationalVersionAttribute("2.1.0.0-7-c68ea866f-20140317_0948")][assembly: AssemblyFileVersionAttribute("2.1.0.0")]
