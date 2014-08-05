f679d62aaa800d33eca86ce5d4a0a04a64e8f919
Ruano <ruano@inventti.com.br>
2014-08-05 11:32:59 -0300

    RS:0: Criação de uma classe estática com constantes dos bancos suportados.

---
+++ src/Net.Sf.Dbdeploy/Configuration/BancosSuportados.cs
@@ -0,0 +1,12 @@
+namespace Net.Sf.Dbdeploy.Configuration
+{
+    public static class BancosSuportados
+    {
+        public const string MSSQL = "mssql";
+        public const string ORACLE = "ora";
+        public const string MYSQL = "mysql";
+        public const string FIREBIRD = "firebird";
+        public const string POSTGRE = "postgre";
+        public const string SYBASE = "sybase";
+    }
+}
