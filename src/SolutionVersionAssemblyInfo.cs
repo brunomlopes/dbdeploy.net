0853ab75fb706b5336c9c6681141593fda930017
Ruano <ruano@inventti.com.br>
2014-09-25 13:55:43 -0300

    RS:0: Criado task psake para gerar uma release do dbdeploy informando a versão desejada.

--- src/SolutionVersionAssemblyInfo.cs
+++ src/SolutionVersionAssemblyInfo.cs
@@ -1,1 +1,1 @@
-// Do not commit the changes to this file.using System.Reflection;[assembly: AssemblyVersionAttribute("2.1.0.0")][assembly: AssemblyInformationalVersionAttribute("2.1.0.0-7-c68ea866f-20140317_1454")][assembly: AssemblyFileVersionAttribute("2.1.0.0")]
+using System.Reflection;[assembly: AssemblyVersionAttribute("3.0")][assembly: AssemblyInformationalVersionAttribute("3.0-20140925_1353")][assembly: AssemblyFileVersionAttribute("3.0")]
