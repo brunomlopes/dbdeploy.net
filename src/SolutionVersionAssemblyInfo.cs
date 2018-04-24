f6abb13ac823231a29f2514e38d204aae9ffded0
Ederson Jasper <ederson@inventti.com.br>
2018-04-24 16:53:37 -0300

    Adicionado provider do oracle8i

--- src/SolutionVersionAssemblyInfo.cs
+++ src/SolutionVersionAssemblyInfo.cs
@@ -1,1 +1,1 @@
-using System.Reflection;[assembly: AssemblyVersionAttribute("3.1")][assembly: AssemblyInformationalVersionAttribute("3.1-20160715_0834")][assembly: AssemblyFileVersionAttribute("3.1")]
+using System.Reflection;[assembly: AssemblyVersionAttribute("18.1.1")][assembly: AssemblyInformationalVersionAttribute("18.1.1-20180424_1613")][assembly: AssemblyFileVersionAttribute("18.1.1")]
